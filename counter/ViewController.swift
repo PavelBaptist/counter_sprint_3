1881abfc5b31c90826fe719f3d7876df3dd9ee3d
Paul Glazachev <glazachev9777@gmail.com>
2023-07-26 14:03:23 +0300

    Добавлена логика ведения лога и возможность просмотра

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -15,16 +15,26 @@ enum EventCounter{
 
 class ViewController: UIViewController {
     
+    // Создание переменных
     private var counter: Int = 0;
+    private var log: String = "";
+    private var infoMode: Bool = false;
     
     @IBOutlet weak var minusUIButton: UIButton!
     @IBOutlet weak var plusUIButton: UIButton!
     @IBOutlet weak var resetUIButton: UIButton!
     @IBOutlet weak var infoUIButton: UIButton!
     @IBOutlet weak var counterUILabel: UILabel!
+    @IBOutlet weak var logUITextView: UITextView!
+    
+    // События
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        refreshViewLog()
     }
 
     @IBAction func btnPlusDidTap(_ sender: Any) {
@@ -40,25 +50,59 @@ class ViewController: UIViewController {
     }
     
     @IBAction func btnInfoDidTap(_ sender: Any) {
+        infoMode.toggle()
+        refreshViewLog()
     }
     
+    // Обработка счетчика
     private func updateCounter(_ eventCounter: EventCounter){
         
         switch eventCounter {
         case .plus:
-            self.counter += 1
+            counter += 1
+            addNewString("значение изменено на +1")
         case .minus:
-            self.counter -= 1
+            counter -= 1
+            addNewString("значение изменено на -1")
         case .reset:
-            self.counter = 0
+            counter = 0
+            addNewString("значение сброшено")
         }
         
-        if self.counter < 0 {
-            self.counter = 0;
+        if counter < 0 {
+            counter = 0
+            addNewString("попытка уменьшить значение счётчика ниже 0")
         }
         
-        counterUILabel.text = String(self.counter)
+        counterUILabel.text = String(counter)
+        
     }
     
+    // Обработка лога
+    private func addNewString(_ text: String){
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = "dd/MM/yyyy HH:mm:ss"
+        
+        let date: String = dateFormatter.string(from: Date())
+        
+        log = "[\(date)]: \(text)\n" + log
+        
+        logUITextView.text = log
+    }
+    
+    private func refreshViewLog(){
+        if infoMode {
+            logUITextView.isHidden = false
+            plusUIButton.frame.size.height = 198;
+            minusUIButton.frame.size.height = 198;
+            infoUIButton.setTitle("Скрыть техническую информацию", for: .normal)
+        } else {
+            logUITextView.isHidden = true
+            plusUIButton.frame.size.height = 464;
+            minusUIButton.frame.size.height = 464;
+            infoUIButton.setTitle("Показать техническую информацию", for: .normal)
+        }
+    }
 }
 
