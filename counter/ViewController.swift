c716ced136b05816336436ab15dcb190a22d504f
Paul Glazachev <glazachev9777@gmail.com>
2023-07-26 13:09:09 +0300

    Добавлена обработка нажатий и основная логика

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum EventCounter{
+    case plus
+    case minus
+    case reset
+}
 
+class ViewController: UIViewController {
+    
+    private var counter: Int = 0;
+    
+    @IBOutlet weak var minusUIButton: UIButton!
+    @IBOutlet weak var plusUIButton: UIButton!
+    @IBOutlet weak var resetUIButton: UIButton!
+    @IBOutlet weak var infoUIButton: UIButton!
+    @IBOutlet weak var counterUILabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnPlusDidTap(_ sender: Any) {
+        updateCounter(EventCounter.plus)
+    }
+    
+    @IBAction func btnMinusDidTap(_ sender: Any) {
+        updateCounter(EventCounter.minus)
+    }
+    
+    @IBAction func btnResetDidTap(_ sender: Any) {
+        updateCounter(EventCounter.reset)
+    }
+    
+    @IBAction func btnInfoDidTap(_ sender: Any) {
+    }
+    
+    private func updateCounter(_ eventCounter: EventCounter){
+        
+        switch eventCounter {
+        case .plus:
+            self.counter += 1
+        case .minus:
+            self.counter -= 1
+        case .reset:
+            self.counter = 0
+        }
+        
+        if self.counter < 0 {
+            self.counter = 0;
+        }
+        
+        counterUILabel.text = String(self.counter)
+    }
+    
 }
 
